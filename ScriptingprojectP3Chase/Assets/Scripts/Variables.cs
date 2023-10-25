85543ac365249d29c37fe0a84345888403df841f
hyper-sonic1 <737509@student.dusd.net>
2023-10-25 10:59:51 -0700

    i did some stuff
    I added some stuff

---
+++ ScriptingprojectP3Chase/Assets/Scripts/Variables.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Variables : MonoBehaviour
+{
+
+    public int myInt = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myInt = MultiplyByTwo(myInt);
+        Debug.Log (myInt);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    int MultiplyByTwo(int num)
+    {
+        int result;
+        result = num * 2;
+        return result;
+    }
+}    
