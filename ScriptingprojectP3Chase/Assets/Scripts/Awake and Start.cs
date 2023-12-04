be4ecd261f1ac5d8dc7d43858e09e8af3fee74c5
hyper-sonic1 <737509@student.dusd.net>
2023-12-04 10:39:01 -0800

    Today I make my Awake Sript
    Yup

---
+++ ScriptingprojectP3Chase/Assets/Scripts/Awake and Start.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AwakeandStart : MonoBehaviour
+{
+
+    void Awake()
+    {
+        Debug.Log("Awake called");
+    }
+    void Start()
+    {
+        Debug.Log("Start called");
+    }
+
+   
+}
