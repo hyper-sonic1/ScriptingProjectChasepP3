e591358d62bf55733ab2e913889414274c3bbaa0
hyper-sonic1 <737509@student.dusd.net>
2024-04-24 12:19:59 -0700

    i added the arrays
    scrfipt

--- ScriptingprojectP3Chase/Assets/Scripts/Arrays.cs
+++ ScriptingprojectP3Chase/Assets/Scripts/Arrays.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class Arrays : MonoBehaviour
 {
-    public GameObject[] objects;    
+    public GameObject[] players;    
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        players = GameObject.FindGameObjectsWithTag("Player");
+
+        for (int i = 0; i < players.Length; i++)
+        {
+            Debug.Log("Player Number"+i+" is named " + players[i].name);
+        }
     }
 
     // Update is called once per frame
